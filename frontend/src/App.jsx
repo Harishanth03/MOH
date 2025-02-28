8ea6580bf6c4c218eec6c524903ead53acb770fa
Harishanth03 <kandeepanharishanth@gmail.com>
2025-02-28 21:06:07 +0530

    add routing for Home, Doctors, Login, About, Contact, MyProfile, and MyAppointment pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Route , Routes} from 'react-router-dom'
+import Home from './Pages/Home'
+import Doctors from './Pages/Doctors'
+import Login from './Pages/Login'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import MyProfile from './Pages/MyProfile'
+import MyAppointment from './Pages/MyAppointment'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/doctors' element = {<Doctors/>}/>
+        <Route path='/doctors/:specialist' element = {<Doctors/>}/>
+        <Route path='/login' element = {<Login/>}/>
+        <Route path='/about' element = {<About/>}/>
+        <Route path='/contact' element = {<Contact/>}/>
+        <Route path='/my-profile' element = {<MyProfile/>}/>
+        <Route path='/my-appointment' element = {<MyAppointment/>}/>
+        <Route path='/my-appointment/:docId' element = {<MyAppointment/>}/>
+      </Routes>
+      
+    </div>
   )
 }
 
