704359b44134818d9319384e63a7a6ce63e2a9a9
Harishanth03 <kandeepanharishanth@gmail.com>
2025-02-28 21:49:37 +0530

    update Navbar styles and enhance active link indicators

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -4,24 +4,24 @@ import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-200'>
+    <div className='flex items-center justify-between text-sm text-gray-600 py-4 mb-5 border-b border-b-gray-200'>
         <img className='w-44 cursor-pointer' src={assets.logo} />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink to={'/'}>
                 <li className='py-1'>HOME</li>
-                <hr  className='border-none outline-none h-0.5 bg-[#0D6EFD] w-3/5 m-auto hidden'/>
+                <hr  className='border-none rounded-full outline-none h-0.5 bg-[#0D6EFD]  m-auto hidden'/>
             </NavLink>
             <NavLink to={'/doctors'}>
                 <li className='py-1'>ALL DOCTORS</li>
-                <hr  className='border-none outline-none h-0.5 bg-[#0D6EFD] w-3/5 m-auto hidden'/>
+                <hr  className='border-none outline-none h-0.5 bg-[#0D6EFD]  m-auto hidden'/>
             </NavLink>
             <NavLink to={'/about'}>
                 <li className='py-1'>ABOUT</li>
-                <hr  className='border-none outline-none h-0.5 bg-[#0D6EFD] w-3/5 m-auto hidden'/>
+                <hr  className='border-none outline-none h-0.5 bg-[#0D6EFD]  m-auto hidden'/>
             </NavLink>
             <NavLink to={'/contact'}>
                 <li className='py-1'>CONTACT</li>
-                <hr className='border-none outline-none h-0.5 bg-[#0D6EFD] w-3/5 m-auto hidden' />
+                <hr className='border-none outline-none h-0.5 bg-[#0D6EFD] m-auto hidden' />
             </NavLink>
         </ul>
         <div>
