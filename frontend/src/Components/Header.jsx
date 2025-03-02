575be196de83cfc65ebfd8debd0b3e16b93ee4cc
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-02 21:42:20 +0530

    feat(Header, SpecializeMenu): enhance navigation and layout for improved user experience

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import {useNavigate} from 'react-router-dom';
 
 const Header = () => {
+
+    const navigate = useNavigate();
+
   return (
     <section id='home'>
 
@@ -13,10 +17,10 @@ const Header = () => {
                 <h1 className="py-6 text-2xl font-bold text-start text-[#023170] sm:text-4xl">From Ministry Of Health</h1>
                 <p className='text-gray-700 font-semibold'>Empowering lives with personalized, innovative healthcare solutions that prioritize well-being, enhance recovery, and redefine patient care through compassion, cutting-edge technology, and a commitment to excellence in every step of the journey.</p>
                 <div>
-                    <button className='px-[18px] py-[10px] mt-4 bg-[#0D6EFD] text-white rounded font-medium'>make an Appointment</button>
+                    <button onClick={() => navigate('/appointment')} className='px-[18px] cursor-pointer py-[10px] mt-4 bg-[#0D6EFD] text-white rounded font-medium'>make an Appointment</button>
                 </div>
             </div>
-            <div className="w-full min-h-[100vw] sm:min-h-[34vw] items-end my-8 bg-no-repeat md:bg-right bg-cover md:bg-contain relative" style={{ backgroundImage: `url(${assets.HomeImage})` }}>
+            <div  className="w-full min-h-[100vw] sm:min-h-[34vw] items-end my-8 bg-no-repeat md:bg-right bg-cover md:bg-contain relative" style={{ backgroundImage: `url(${assets.HomeImage})` }}>
             </div>
         </div>
 
