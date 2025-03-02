0ceb5a8a22645a313f6db358b7cae840f62d4fe7
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-02 21:35:37 +0530

    feat(SpecializeMenu): update layout for improved display of specialty links

--- frontend/src/Components/SpecializeMenu.jsx
+++ frontend/src/Components/SpecializeMenu.jsx
@@ -4,11 +4,11 @@ import {Link} from 'react-router-dom'
 
 const SpecializeMenu = () => {
   return (
-    <>
+    
         <section className='w-full mt-10'>
             <h1 className='text-2xl font-bold text-center text-gray-900 sm:text-4xl'>Find your doctor by speciality</h1>
             <p className='text-sm text-gray-500 text-center mt-6'>Discover top-rated doctors across various specialties and book your appointment with ease. Your health, your choice!</p>
-            <div className='flex flex-wrap justify-center gap-4 my-5'>
+            <div className='flex flex-row justify-center gap-4 my-5'>
             {
                 specialityData.map((item  , index) => (
                     <Link className='w-40 p-4 my-5 bg-gray-100 rounded-md text-center cursor-pointer' key={index} to={`/doctors/${item.speciality}`}>
@@ -19,7 +19,6 @@ const SpecializeMenu = () => {
             }
             </div>
         </section>
-    </>
   )
 }
 
