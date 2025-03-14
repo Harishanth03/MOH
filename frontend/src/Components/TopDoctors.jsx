98dad12f9ba3b731ae17b39a47dd4679c34ed610
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-15 00:46:27 +0530

    feat(TopDoctors): add navigation to appointment page and enhance doctor details display

--- frontend/src/Components/TopDoctors.jsx
+++ frontend/src/Components/TopDoctors.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { doctors } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const TopDoctors = () => {
+
+    const navigate = useNavigate();
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-2xl font-bold text-center text-gray-900 sm:text-4xl'>Doctors To Book</h1>
@@ -10,21 +13,21 @@ const TopDoctors = () => {
         <div className='w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
             {
                 doctors.slice(0,10).map((item , index) => (
-                    <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                    <div onClick={() => navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                         <img className='' src={item.image} alt="" />
                         <div className='p-4'>
                            <div className='flex items-center gap-2 text-sm text-center text-green-500'>
                                 <p className='w-2 h-2 bg-green-500 rounded-full'></p>
                                 <p>Available</p>
                            </div>
-                            <p>{item.name}</p>
-                            <p>{item.speciality}</p>
+                            <p className='text-gray-900 text-md font-medium'>{item.name}</p>
+                            <p className='text-gray-600 text-sm '>{item.speciality}</p>
                         </div>
                     </div>
                 ))
             }
         </div>
-        <button>Browse More</button>
+        <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full cursor-pointer'>Browse More</button>
     </div>
   )
 }
