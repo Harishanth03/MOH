3d3204dbbf39e6fd71c7e3fcfad9077cb2e9c920
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-15 01:05:14 +0530

    feat(TopDoctors, Context): integrate AppContext for doctor data and update navigation button

--- frontend/src/Components/TopDoctors.jsx
+++ frontend/src/Components/TopDoctors.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import { doctors } from '../assets/assets'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import appContextProvider, { AppContext } from '../Context/AppContext.jsx';
 
 const TopDoctors = () => {
 
     const navigate = useNavigate();
+
+    const {doctors} = useContext(AppContext)
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-2xl font-bold text-center text-gray-900 sm:text-4xl'>Doctors To Book</h1>
@@ -27,7 +29,7 @@ const TopDoctors = () => {
                 ))
             }
         </div>
-        <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full cursor-pointer'>Browse More</button>
+        <button onClick={() => {navigate('/doctors');scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full cursor-pointer'>Browse More</button>
     </div>
   )
 }
