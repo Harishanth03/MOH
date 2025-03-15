539c5e52bf059d590f336c21cc6544a2d8f75168
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-15 13:19:19 +0530

    feat(Footer): restructure Footer component layout with sections for improved organization

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+        
+        <div>
+
+            {/* ======================================= Left Section of Footer ================================== */}
+            <div>
+
+                
+
+            </div>
+
+            {/* ======================================= Center Section of Footer ================================== */}
+            <div>
+
+                
+            </div>
+
+            {/* ======================================= Right Section of Footer ================================== */}
+            <div>
+
+                
+            </div>
+
+        </div>
+
+    </div>
   )
 }
 
