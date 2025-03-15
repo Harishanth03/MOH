a3b073fe4a203de5cc65a78c071b364968fce93a
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-15 18:20:51 +0530

    feat(Doctors): implement useParams to retrieve specialty from URL

--- frontend/src/Pages/Doctors.jsx
+++ frontend/src/Pages/Doctors.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Doctors = () => {
+
+  const { speciality} = useParams();
   return (
-    <div>Doctors</div>
+    <div>
+      
+    </div>
   )
 }
 
