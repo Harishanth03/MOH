fb56d2500ceb977ab81ab7133974fdcee7fa01c3
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-19 20:41:13 +0530

    feat(MyProfile): add additional user data fields including email, phone, address, gender, and date of birth

--- frontend/src/Pages/MyProfile.jsx
+++ frontend/src/Pages/MyProfile.jsx
@@ -6,6 +6,14 @@ const MyProfile = () => {
   const [userData , setUserData] = useState({
     name : "Harishanth",
     image : assets.user,
+    email : "Harishanth08@gmail.com",
+    phone : "1234567890",
+    address : {
+      line1: "233 , Anbuvalipuram",
+      line2: "Trincomalee , Sri Lanka"
+    },
+    gender : "Male",
+    dob : "2000-07-03",
   })
   return (
     <section className='w-full min-h-screen'>
