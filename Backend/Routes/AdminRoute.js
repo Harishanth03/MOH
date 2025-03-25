b1728aea430027559a70b033f33a80fc9f2ad45c
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-25 20:08:51 +0530

    feat(Backend): update admin login to return access token and add authentication middleware to addDoctor route

--- Backend/Routes/AdminRoute.js
+++ Backend/Routes/AdminRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { addDoctor, adminLogin } from '../Controllers/AdminController.js';
 import upload from '../Middlewares/Multer.js';
+import authAdmin from '../Middlewares/AuthAdmin.js';
 
 
 const adminRouter = express.Router();
 
-adminRouter.post('/add-doctor',upload.single('image'), addDoctor)
+adminRouter.post('/add-doctor',authAdmin,upload.single('image'), addDoctor)
 
 adminRouter.post('/admin-login', adminLogin)
 
