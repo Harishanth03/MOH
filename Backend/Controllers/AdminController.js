b75a59b00d46b53e57669b8fbe1f99642e53c71a
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-25 18:29:40 +0530

    feat(Backend): implement addDoctor function in AdminController for adding new doctor details

---
+++ Backend/Controllers/AdminController.js
@@ -0,0 +1,18 @@
+
+
+
+
+//================================================ Add Doctor Function ===================================================
+
+const addDoctor = async(req , res) => {
+
+    try 
+    {
+
+        const {name , email , password , speciality , degree , experience , about , available , address } = req.body;
+        
+    } catch (error) 
+    {
+        
+    }
+}
