6fc08f73fdcae7ce1c040932ef64f966d194df58
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-25 16:57:32 +0530

    feat(Backend): set up server with express, cors, and dotenv configuration

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,2 +1,26 @@
-import express from 'express';
+import express from 'express'
+
+import cors from 'cors'
+
+import 'dotenv/config'
+
+//=============================================== App Config ========================================================
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+//=============================================== Middleware ========================================================
+
+app.use(express.json())
+
+app.use(cors())
+
+//=============================================== Api End Point =====================================================
+
+app.get('/', (req , res) => {
+    res.send("API WORKING")
+})
+
+app.listen(port , () => console.log('Server Started'), port);
 
