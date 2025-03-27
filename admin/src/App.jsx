dd3eefedc39fe602f432d4ab92ab2beadc92ddb6
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-27 10:13:26 +0530

    feat(App): add Navbar component and update App layout with background styling

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,15 +2,17 @@ import React, { useContext } from 'react'
 import Login from './Pages/Login'
 import { ToastContainer, toast } from 'react-toastify'
 import { AdminContext } from './Context/AdminContext'
+import Navbar from './Components/Navbar.Jsx'
 
 const App = () => {
 
   const{aToken} = useContext(AdminContext)
 
   return aToken ? (
-    <div>
+    <div className='bg-[#F8F9FD]'>
 
       <ToastContainer/>
+      <Navbar/>
 
     </div>
   ):
