3f94074228a40cbbfb313cb8e91dcad68309d70d
Harishanth03 <kandeepanharishanth@gmail.com>
2025-03-28 13:43:23 +0530

    feat(DoctorsList): integrate AdminContext to access doctors and authentication token

--- admin/src/Pages/Admins/DoctorsList.jsx
+++ admin/src/Pages/Admins/DoctorsList.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AdminContext } from '../../Context/AdminContext'
 
 const DoctorsList = () => {
 
-  
+  const {doctors , aToken , getAllDoctors} = useContext(AdminContext);
 
   return (
 
