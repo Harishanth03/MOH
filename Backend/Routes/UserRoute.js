9101591a21fbaded6e183bc061106d72e2c0e00f
Harishanth03 <kandeepanharishanth@gmail.com>
2025-04-03 19:35:47 +0530

    feat: add user profile retrieval functionality with authentication middleware

--- Backend/Routes/UserRoute.js
+++ Backend/Routes/UserRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { loginUser, registerUser } from '../Controllers/UserController.js';
+import { getProfile, loginUser, registerUser } from '../Controllers/UserController.js';
+import authUser from '../Middlewares/AuthUser.js';
 
 const userRouter  = express.Router();
 
 userRouter.post('/register' , registerUser);
 userRouter.post('/login' , loginUser);
+userRouter.get('/get-profile', authUser , getProfile)
 
 
 export default userRouter;
