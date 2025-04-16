6b3f7d68454b4139d8bb2af53e90c2ba2f62a887
Harishanth03 <kandeepanharishanth@gmail.com>
2025-04-16 08:09:09 +0530

    feat: update Sidebar component for responsive display and enhance DoctorAppointment layout

--- admin/src/Pages/Doctors/DoctorAppointment.jsx
+++ admin/src/Pages/Doctors/DoctorAppointment.jsx
@@ -13,7 +13,28 @@ const DoctorAppointment = () => {
 
   }, [dToken])
   return (
-    <div>DoctorAppointment</div>
+
+    <div className='w-full max-w-6xl m-5'>
+      
+      <p className='mb-3 text-lg font-medium'>All Appointments</p>
+
+      <div className='bg-white border border-gray-300 rounded text-sm max-h-[80vh] overflow-y-scroll min-h-[50vh]'>
+
+        <div className='max-sm:hidden grid grid-cols-[0.5fr_2fr_1.5fr_1fr_2fr_2fr] gap-1 py-3 px-6 border-b border-gray-300'> 
+
+          <p>#</p>
+          <p>Patient Details</p>
+          <p>Status</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Action</p>
+
+        </div>
+
+      </div>
+
+    </div>
+
   )
 }
 
