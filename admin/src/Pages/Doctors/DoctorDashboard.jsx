41a1a37b9612ce40f78de458c4396a024c00d923
Harishanth03 <kandeepanharishanth@gmail.com>
2025-04-25 11:53:50 +0530

    feat: enhance DoctorDashboard with appointment cancellation and completion functionality; update data handling for user details

--- admin/src/Pages/Doctors/DoctorDashboard.jsx
+++ admin/src/Pages/Doctors/DoctorDashboard.jsx
@@ -4,8 +4,9 @@ import { assets } from "../../assets/assets";
 import { AppContext } from "../../Context/AppContext";
 
 const DoctorDashboard = () => {
-  const { dashData, setDashData, getDashData, dToken } = useContext(DoctorContext);
-  const {slotDateFormat} = useContext(AppContext);
+  const { dashData, setDashData, getDashData, dToken , CancleAppointment , completeAppointment} =
+    useContext(DoctorContext);
+  const { slotDateFormat } = useContext(AppContext);
 
   useEffect(() => {
     if (dToken) {
@@ -18,17 +19,6 @@ const DoctorDashboard = () => {
     dashData && (
       <div className="m-5">
         <div className="flex flex-wrap gap-3">
-          <div className="flex item-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-blue-500 cursor-pointer hover:scale-105 transition-all">
-            <img className="w-14" src={assets.doctor_icon} alt="" />
-
-            <div>
-              <p className="text-xl font-semibold text-gray-600">
-                {dashData.doctors}
-              </p>
-              <p className="text-gray-400">Doctors</p>
-            </div>
-          </div>
-
           <div className="flex item-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-blue-500 cursor-pointer hover:scale-105 transition-all">
             <img className="w-14" src={assets.appointments_icon} alt="" />
 
@@ -50,26 +40,6 @@ const DoctorDashboard = () => {
               <p className="text-gray-400">Patients</p>
             </div>
           </div>
-
-          <div className="flex item-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-blue-500 cursor-pointer hover:scale-105 transition-all">
-            <img className="w-14" src={assets.donation_icon} alt="" />
-
-            <div>
-              <p className="text-xl font-semibold text-gray-600">
-                රු.{dashData.totaldonations}
-              </p>
-              <p className="text-gray-400">Donations</p>
-            </div>
-          </div>
-
-          <div className="flex item-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-blue-500 cursor-pointer hover:scale-105 transition-all">
-            <img className="w-14" src={assets.total_beds} alt="" />
-
-            <div>
-              <p className="text-xl font-semibold text-gray-600">230</p>
-              <p className="text-gray-400">Beds Available</p>
-            </div>
-          </div>
         </div>
 
         <div className="bg-white ">
@@ -89,26 +59,49 @@ const DoctorDashboard = () => {
               >
                 <img
                   className="rounded-full w-12"
-                  src={item.docData.image}
+                  src={item.userData.image}
                   alt=""
                 />
 
                 <div className="flex-1 text-sm">
-                  <p className="text-gray-800">{item.docData.name}</p>
+                  <p className="text-gray-800">{item.userData.name}</p>
                   <p className="text-gray-500">
                     {slotDateFormat(item.slotDate)}
                   </p>
                 </div>
 
                 {item.cancelled ? (
-                  <p className="text-red-500 text-xs font-medium">Cancelled</p>
+                  item.isCompleted ? (
+                    <p className="text-red-500 text-sm font-medium">
+                      Appointment Cancelled by patient
+                    </p>
+                  ) : (
+                    <p className="text-red-500 text-sm font-medium">
+                      Appointment Cancelled by doctor
+                    </p>
+                  )
+                ) : item.isCompleted ? (
+                  <button
+                    className="px-3 cursor-pointer py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+                    onClick={() => handleWriteReport(item._id)}
+                  >
+                    Write Report
+                  </button>
                 ) : (
-                  <img
-                    onClick={() => cancleAppointment(item._id)}
-                    className="w-5"
-                    src={assets.delete_icon}
-                    alt=""
-                  />
+                  <>
+                    <img
+                      onClick={() => CancleAppointment(item._id)}
+                      className="w-10 cursor-pointer"
+                      src={assets.cancel_icon}
+                      alt="Cancel"
+                    />
+                    <img
+                      onClick={() => completeAppointment(item._id)}
+                      className="w-10 cursor-pointer"
+                      src={assets.tick_icon}
+                      alt="Complete"
+                    />
+                  </>
                 )}
               </div>
             ))}
